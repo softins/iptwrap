43ba10e6416ef16336c9a4d18cd16c29523289cd
Tony Mountifield <tony@mountifield.org>
2023-07-14 11:55:35 +0100

    Improvements to ipaddr.php

--- ipaddr.php
+++ ipaddr.php
@@ -1,4 +1,5 @@
 <?php
+
 	$iptwrap = '/usr/local/bin/iptwrap';
 	$chain = 'Remote-Workers';
 
@@ -6,42 +7,75 @@
 
 	if (@$_POST['submit'] == 'Add IP') {
 		# strip out invalid characters
-		$ret = exec("$iptwrap -c $chain -a $myip", $output, $status);
+		$cmd = "$iptwrap -c $chain -a $myip";
+		$ret = exec($cmd, $output, $status);
+		error_log("cmd=\"$cmd\", ret=$ret, output=" . join('|',$output) . ", status=$status");
 		if ($status == 0) {
 			header('Location: http://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']);
 			exit;
 		}
 	} elseif (@$_POST['submit'] == 'Remove IP') {
 		# strip out invalid characters
-		$ret = exec("$iptwrap -c $chain -d $myip", $output, $status);
+		$cmd = "$iptwrap -c $chain -d $myip";
+		$ret = exec($cmd, $output, $status);
+		error_log("cmd=\"$cmd\", ret=$ret, output=" . join('|',$output) . ", status=$status");
 		if ($status == 0) {
 			header('Location: http://'.$_SERVER['SERVER_NAME'].$_SERVER['PHP_SELF']);
 			exit;
 		}
 	}
 
-	$ret = exec("$iptwrap -c $chain -l $myip", $output, $status);
+	$cmd = "$iptwrap -c $chain -l $myip";
+	$ret = exec($cmd, $output, $status);
+	error_log("cmd=\"$cmd\", ret=$ret, output=" . join('|',$output) . ", status=$status");
 ?>
+<!DOCTYPE html>
 <html>
 <head>
-<title>System IP address access</title>
+<title>IP address access</title>
+<style type="text/css"><!--
+body {
+	font-family: Verdana, Arial, Helvetical, sans-serif;
+	text-align: center;
+}
+h1 {
+	text-align: center;
+}
+td.r {
+	text-align: right;
+}
+div.wrap {
+	display: inline-block;
+	text-align: left;
+	*display: inline;
+	zoom: 1;
+}
+table.main {
+	border: 0;
+}
+table.main td {
+	padding-right: 2em;
+	text-align: center;
+}
+--></style>
 </head>
 <body>
+<div class="wrap">
+<h1>IP address access</h1>
 <form method="post">
-<table border=0 width="700px">
-<tr><td>
-<h1>Meet&Talk&trade; IP address access</h1></td></tr>
+<table class="main">
 <tr><td><H2></h2></td></tr>
 <?php if (@$status != 0) { ?>
-<tr><td><h3 style="color: red">Your IP address <?= $myip ?> is NOT enabled for access</h3></td></tr>
+<tr><td><h3 style="color: red">Your IP address <?= $myip ?> is NOT enabled for remote access</h3></td></tr>
 <tr><td><h3>Click the button below to enable your IP address</h3></td></tr>
 <tr><td align="center"><input type="submit" name="submit" value="Add IP" /></td></tr>
 <?php } else { ?>
-<tr><td><h3 style="color: green">Your IP address <?= $myip ?> is enabled for access</h3></td></tr>
+<tr><td><h3 style="color: green">Your IP address <?= $myip ?> is enabled for remote access</h3></td></tr>
 <tr><td><h3>Click the button below to disable your IP address</h3></td></tr>
 <tr><td align="center"><input type="submit" name="submit" value="Remove IP" /></td></tr>
 <?php } ?>
 </table>
 </form>
+</div>
 </body>
 </html>
